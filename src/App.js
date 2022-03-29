18bb8de9a127a329744982d94bd589622a7bd4f0
Tom-stack3 <tommy.zaft1@gmail.com>
2022-03-29 16:47:10 +0300

    Created React app, logo and ico files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          MaKore
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
