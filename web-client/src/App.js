f30565f1462a5323e0a912a6e0ec73905c6951c5
Tommy <tommy.zaft1@gmail.com>
2022-03-30 18:35:19 +0300

    Added basic routing

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          MaKore
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            MaKore
+          </p>
+        </header>
+        <Routes>
+          <Route path='/' element={
+            // Should check if user is signed in or not. If not, redirect to login page.
+            // If signed in, render the Main component.
+            <>
+              <p>Main page</p>
+            </>
+          } />
+          <Route path='/login' element={
+            // Should check if user is signed in or not. If signed in, redirect to main page.
+            // If not, render the Login component.
+            <>
+              <p>Login page</p>
+            </>
+          } />
+          <Route path='/signup' element={
+            // Should check if user is signed in or not. If signed in, redirect to main page.
+            // If not, render the Signup component.
+            <>
+              <p>Signup page</p>
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
