7002a3c20fa90aeb9dbe09cb660ee4447ef11b98
OmerShoulstein <omershoulstein@gmail.com>
2022-04-04 18:22:05 +0300

    Buttons are working

--- web-client/src/components/MainPage.js
+++ web-client/src/components/MainPage.js
@@ -1,10 +1,16 @@
+import {Link} from 'react-router-dom';
+
 const MainPage = () => {
     return (
         <div id="form-frame">
             <h1 className="form-title">Join MaKore today!</h1>
-            <button className="button main-page-button">Sign Up</button>
+            <Link to="/signup">
+                <button className="button main-page-button">Sign Up</button>
+            </Link>
             <h1 className="form-title">Already have an account?</h1>
-            <button className="button main-page-button">Login</button>
+            <Link to="/login">
+                <button className="button main-page-button">Login</button>
+            </Link>
         </div>
     )
 };
