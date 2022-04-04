036be97fc7f2b8d0259adea2518c534e8f4d27e6
OmerShoulstein <omershoulstein@gmail.com>
2022-04-04 17:42:35 +0300

    minor

--- web-client/src/components/MainPage.js
+++ web-client/src/components/MainPage.js
@@ -2,9 +2,9 @@ const MainPage = () => {
     return (
         <div id="form-frame">
             <h1 className="form-title">Join MaKore today!</h1>
-            <button className="button main-button">Sign Up</button>
+            <button className="button main-page-button">Sign Up</button>
             <h1 className="form-title">Already have an account?</h1>
-            <button className="button main-button">Login</button>
+            <button className="button main-page-button">Login</button>
         </div>
     )
 };
