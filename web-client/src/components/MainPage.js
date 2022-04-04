dda76304958c216e872a065bc0185c8d0ce276ff
OmerShoulstein <omershoulstein@gmail.com>
2022-04-04 17:15:25 +0300

    added join us prompt

--- web-client/src/components/MainPage.js
+++ web-client/src/components/MainPage.js
@@ -1,8 +1,11 @@
 const MainPage = () => {
     return (
-        <>
-            <h1>Main Page</h1>
-        </>
+        <div id="form-frame">
+            <h1 className="form-title">Join MaKore today!</h1>
+            <button className="button main-button">Sign Up</button>
+            <h1 className="form-title">Already have an account?</h1>
+            <button className="button main-button">Login</button>
+        </div>
     )
 };
 
