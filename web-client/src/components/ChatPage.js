9a904d3b708c9995eb85524f6e02477755443fc3
Tom Ben-Dor <36517134+TomBenDor@users.noreply.github.com>
2022-04-10 20:49:29 +0300

    Basic layout
    * Plus button
    * Typing area

--- web-client/src/components/ChatPage.js
+++ web-client/src/components/ChatPage.js
@@ -1,32 +1,55 @@
+import {useRef} from "react";
+
 const ChatPage = (user) => {
     const openContactDialog = () => {
         console.log("openContactDialog");
     };
 
+    const messageBox = useRef(null);
+
+    const typing = () => {
+        document.getElementById("send-button").disabled = messageBox.current.value.length === 0;
+    }
+
     return (
         <div id="content-frame">
             <div className="chat-section">
                 <div className="chat-section-header">
+                    <span className="profile-pic">
+                        <img
+                            src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+                            className="center" alt="profile-pic"/>
+                    </span>
+                </div>
+                <div className="chat-section-messages">
+
+                </div>
+                <div className="chat-section-input-bar">
+                    <input ref={messageBox} id="message-input" type="text" placeholder="Type a message..."
+                           onChange={typing}/>
+                    <button id="send-button">Send</button>
                 </div>
             </div>
             <div className="contacts-section">
                 <div className="contacts-section-header">
+                <span className="contacts-section-header-user">
+                    <span className="profile-pic">
+                        <img
+                            src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+                            className="center" alt="profile-pic"/>
+                    </span>
                     <span className="contacts-section-header-title">
                         {user.user.displayName}
                     </span>
+                </span>
                     <span className="contacts-section-header-controls">
                         <a className="add-contact-button" onClick={openContactDialog}>
-                            <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px"
-                                 height="50px">
-                                <path
-                                    d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24 13 L 24 24 L 13 24 L 13 26 L 24 26 L 24 37 L 26 37 L 26 26 L 37 26 L 37 24 L 26 24 L 26 13 L 24 13 z"/>
-                            </svg>
+                            <img src={"plus.svg"} alt="Add contact"/>
                         </a>
-                    </span>
+                </span>
                 </div>
             </div>
-        </div>
-    );
+        </div>);
 };
 
 export default ChatPage;
