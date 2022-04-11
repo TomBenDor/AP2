d61e7562431e3396879ad7de182f53decb127c87
Tom Ben-Dor <36517134+TomBenDor@users.noreply.github.com>
2022-04-11 11:35:00 +0300

    Fixed

--- web-client/src/components/SvgAnimation/SvgAnimation.js
+++ web-client/src/components/SvgAnimation/SvgAnimation.js
@@ -3,7 +3,7 @@ import "./SvgAnimation.css";
 
 const SvgAnimation = () => {
     return (
-        <div className="center">
+        <div className="center" style={{width: "100%", height: "100%"}}>
             <LandingPageSvg/>
         </div>
     )
