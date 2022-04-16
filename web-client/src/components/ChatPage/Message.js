54a30965150a2492eb852c9aef7c413f597b01fe
Tom Ben-Dor <36517134+TomBenDor@users.noreply.github.com>
2022-04-16 22:07:08 +0300

    Sorting contacts list by last message time

--- web-client/src/components/ChatPage/Message.js
+++ web-client/src/components/ChatPage/Message.js
@@ -5,7 +5,11 @@ const Message = ({message}) => {
         <div className={"message-" + message.sender}>
             <div className="message-bubble">
                 <div className="message-text"><p>{message.text}</p></div>
-                <div className="message-timestamp">{message.timestamp}</div>
+                <div className="message-timestamp">{new Date(message.timestamp).toLocaleTimeString("en-US", {
+                    hour12: false,
+                    hour: "numeric",
+                    minute: "numeric"
+                })}</div>
             </div>
         </div>
     );
