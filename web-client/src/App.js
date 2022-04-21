4d24c688a18dfa5576530a20600d797d58fcf42e
Tommy Zaft <76645845+Tom-stack3@users.noreply.github.com>
2022-04-21 15:24:41 +0300

    Move database to JSON file (#113)

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -4,68 +4,14 @@ import SignInForm from "./components/Auth/SignIn/SignInForm";
 import SignUpForm from "./components/Auth/SignUp/SignUpForm";
 import ChatPage from "./components/ChatPage/ChatPage";
 import LandingPage from "./components/LandingPage/LandingPage";
+let DB = require('./database.json');
 
 const App = () => {
     // Current signed in user
-    // currentUser: { username: "", displayName: "", profilePicture: "" }
     const [currentUser, setCurrentUser] = useState(null);
-    // Users from the database (for now it's just hardcoded)
-    const [users, setUsers] = useState([
-        {
-            "username": "user123",
-            "password": "pass123",
-            "displayName": "Coolest dude ever",
-            "profilePicture": "profile_picture.png"
-        },
-        {
-            "username": "user456",
-            "password": "pass456",
-            "displayName": "Lion",
-            "profilePicture": "profile_picture.png"
-        },
-    ]);
 
-    const [contacts, setContacts] = useState([
-        {
-            id: 0,
-            username: 'Panda',
-            name: 'Panda Bear',
-            profilePicture: 'panda.jpg',
-            unreadMessages: 1,
-            messages: [
-                {
-                    id: 1,
-                    sender: 'left',
-                    text: 'Do you got any bamboo left?',
-                    timestamp: '4/16/2022, 14:49:00',
-                    type: 'text'
-                },
-                {
-                    id: 2,
-                    sender: 'right',
-                    text: 'Not for you, sorry.',
-                    timestamp: '4/16/2022, 15:01:00',
-                    type: 'text'
-                }
-            ]
-        },
-        {
-            id: 1,
-            username: 'Koala',
-            name: 'Koala Bear',
-            profilePicture: 'koala.jpg',
-            unreadMessages: 1,
-            messages: [
-                {
-                    id: 1,
-                    sender: 'right',
-                    text: 'Wanna have a sleepover?',
-                    timestamp: '4/16/2022, 12:32:00',
-                    type: 'text'
-                },
-            ]
-        },
-    ]);
+    const [users, setUsers] = useState(DB.users);
+    const [contacts, setContacts] = useState(DB.contacts);
 
     return (
         <Router>
