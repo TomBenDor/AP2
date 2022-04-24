cdf77a65ad26b17cf3092132d24883c88bfc09b0
Tommy <tommy.zaft1@gmail.com>
2022-04-24 20:01:33 +0300

    Changed moon icon

--- web-client/src/components/ChatPage/ToggleTheme.js
+++ web-client/src/components/ChatPage/ToggleTheme.js
@@ -2,7 +2,7 @@ const ToggleTheme = ({ theme, setTheme }) => {
     return (
         <>
             <button className="icon-button" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-                {theme === 'dark' ? <i className="bi bi-sun"></i> : <i className="bi bi-moon"></i>}
+                {theme === 'dark' ? <i className="bi bi-sun"></i> : <i className="bi bi-moon-stars"></i>}
             </button>
         </>
     );
