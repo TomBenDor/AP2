7a3584ae8df486dd12cf871b7d0ace04ca27ec70
Tommy <Tom-stack3@users.noreply.github.com>
2022-05-14 21:04:49 +0300

    Create Contacts Controller

---
+++ web-api/Controllers/ContactsController.cs
@@ -0,0 +1,110 @@
+using Microsoft.AspNetCore.Mvc;
+using class_library;
+
+namespace web_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class ContactsController : ControllerBase
+{
+    // private readonly IUsersRepository _contactRepository;
+    
+    // public ContactsController(IUsersRepository contactRepository)
+    // {
+    //     _contactRepository = contactRepository;
+    // }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        // return Ok(_contactRepository.GetAll());
+        return Ok("All Contacts of current user");
+    }
+    
+    [HttpPost]
+    public IActionResult Post([FromBody] User contact)
+    {
+        // if (contact == null)
+        // {
+        //     return BadRequest();
+        // }
+        // _contactRepository.Add(contact);
+        // return CreatedAtAction("Get", new { id = contact.Id }, contact);
+        return Ok("Create new Contact");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        // var contact = _contactRepository.GetById(id);
+        // if (contact == null)
+        // {
+        //     return NotFound();
+        // }
+        // return Ok(contact);
+        return Ok("Get Contact with id: " + id);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] User contact)
+    {
+        // if (contact == null || contact.Id != id)
+        // {
+        //     return BadRequest();
+        // }
+        // var contactToUpdate = _contactRepository.GetById(id);
+        // if (contactToUpdate == null)
+        // {
+        //     return NotFound();
+        // }
+        // _contactRepository.Update(contactToUpdate, contact);
+        // return NoContent();
+        return Ok("Update Contact with id: " + id);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Ok("Delete Contact with id: " + id);
+    }
+    
+    [HttpGet("{id}/messages")]
+    public IActionResult GetMessages(int id)
+    {
+        return Ok("Get Messages of Contact with id: " + id);
+    }
+    [HttpPost("{id}/messages")]
+    public IActionResult PostNewMessage(int id, [FromBody] Message message)
+    {
+        return Ok("Create new Message sent to Contact with id: " + id);
+    }
+    
+    [HttpGet("{id}/messages/{id2}")]
+    public IActionResult GetMessage(int id, int id2)
+    {
+        return Ok("Get Message with id: " + id2 + " sent to Contact with id: " + id);
+    }
+    [HttpPut("{id}/messages/{id2}")]
+    public IActionResult PutMessage(int id, int id2, [FromBody] Message message)
+    {
+        return Ok("Update Message with id: " + id2 + " sent to Contact with id: " + id);
+    }
+    [HttpDelete("{id}/messages/{id2}")]
+    public IActionResult DeleteMessage(int id, int id2)
+    {
+        return Ok("Delete Message with id: " + id2 + " sent to Contact with id: " + id);
+    }
+    
+    [HttpPost("/invitations")]
+    public IActionResult PostInvitation([FromBody] Invitation invitation)
+    {
+        return Ok("Create new Invitation");
+    }
+    
+    [HttpPost("/transfer")]
+    public IActionResult PostTransfer([FromBody] Transfer transfer)
+    {
+        return Ok("Transfer new message to user on a remote server");
+    }
+}
