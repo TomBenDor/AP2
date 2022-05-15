39b5546d1696f6c9e8d1da8a7e5959a19f255859
OmerShoulstein <omershoulstein@gmail.com>
2022-05-15 19:23:01 +0300

    Created Review page and CRUD pages.

--- reviews-website/Models/Review.cs
+++ reviews-website/Models/Review.cs
@@ -4,6 +4,14 @@ namespace server.Models;
 
 public class Review
 {
+    public Review(int id, String comment, int rating, String username, DateTime date)
+    {
+        Id = id;
+        Comment = comment;
+        Rating = rating;
+        Username = username;
+        Date = date;
+    }
     public int Id { get; set; }
     [Required] public string Comment { get; set; }
     [Range(1, 5)] public int Rating { get; set; }
---
+++ reviews-website/services/ReviewService.cs
@@ -0,0 +1,47 @@
+using server.Models;
+
+namespace server.services;
+
+public class ReviewService : IReviewService
+{
+    private static List<Review> _reviews = new ();
+    
+
+    public void Create(String name, String comment, int rating)
+    {
+        int newId = _reviews.Count + 1;
+        Review review = new Review(newId, comment, rating, name, DateTime.Now);
+        _reviews.Add(review);
+    }
+
+    public Review? Get(int id)
+    {
+        return _reviews.FirstOrDefault(r => r.Id == id);
+    }
+
+    public List<Review> GetAll()
+    {
+        return _reviews;
+    }
+
+    public void Update(int id, String name, String comment, int rating)
+    {
+        Review review = _reviews.FirstOrDefault(r => r.Id == id);
+        if (review != null)
+        {
+            review.Username = name;
+            review.Comment = comment;
+            review.Rating = rating;
+            review.Date = DateTime.Now;
+        }
+    }
+
+    public void Delete(int id)
+    {
+        var reviewToDelete = _reviews.FirstOrDefault(r => r.Id == id);
+        if (reviewToDelete != null)
+        {
+            _reviews.Remove(reviewToDelete);
+        }
+    }
+}
