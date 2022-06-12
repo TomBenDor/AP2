ff4d4d8b4848b4025f19812c27b40b7ded9e2d8b
Tommy <Tom-stack3@users.noreply.github.com>
2022-06-12 19:06:17 +0300

    Create Sign Up screen

---
+++ android-client/app/src/main/java/com/example/makore/auth/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.makore.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.makore.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
