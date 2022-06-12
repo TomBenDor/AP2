d41687ef1e727dfe09316ba0a6ceef5b9b3bb273
Tommy <Tom-stack3@users.noreply.github.com>
2022-06-12 22:18:39 +0300

    Set Sign In as startup activity Redirect to MainActivity after Sign In

--- android-client/app/src/main/java/com/example/makore/auth/SignInActivity.java
+++ android-client/app/src/main/java/com/example/makore/auth/SignInActivity.java
@@ -1,11 +1,11 @@
 package com.example.makore.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.makore.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.makore.MainActivity;
 import com.example.makore.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
@@ -21,6 +21,30 @@ public class SignInActivity extends AppCompatActivity {
         binding.linkToSignUp.setOnClickListener(view -> {
             Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
             startActivity(intent);
+        });
+
+        binding.signInButton.setOnClickListener(view -> {
+            // Get username and password from the UI
+            String username = binding.editTextUsername.getText().toString();
+            String password = binding.editTextPassword.getText().toString();
+            if (username.isEmpty()) {
+                // Show error message
+                binding.editTextUsername.setError("Username is empty");
+            } else if (password.isEmpty()) {
+                // Show error message
+                binding.editTextPassword.setError("Password is empty");
+            } else {
+                // Check if the username and password is correct
+                if (username.equals("admin") && password.equals("admin")) {
+                    // Go to the main screen
+
+                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Show error message
+                    binding.editTextUsername.setError("One of the fields is invalid");
+                }
+            }
         });
     }
 }
