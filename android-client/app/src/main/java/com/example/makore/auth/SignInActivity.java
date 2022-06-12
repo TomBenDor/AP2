2ceca07a9c85a2bd2ecfe8ac0324070431543dae
Tommy <Tom-stack3@users.noreply.github.com>
2022-06-12 19:47:21 +0300

    Use binding and link between Sign up and Sign in

--- android-client/app/src/main/java/com/example/makore/auth/SignInActivity.java
+++ android-client/app/src/main/java/com/example/makore/auth/SignInActivity.java
@@ -2,15 +2,25 @@ package com.example.makore.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.makore.R;
+import com.example.makore.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.linkToSignUp.setOnClickListener(view -> {
+            Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- android-client/app/src/main/java/com/example/makore/auth/SignUpActivity.java
+++ android-client/app/src/main/java/com/example/makore/auth/SignUpActivity.java
@@ -2,15 +2,25 @@ package com.example.makore.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.makore.R;
+import com.example.makore.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.linkToSignIn.setOnClickListener(view -> {
+            Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+            startActivity(intent);
+        });
     }
 }
