e1b34d1050f742754def2740befc46ae32611559
Tommy Zaft <76645845+Tom-stack3@users.noreply.github.com>
2022-06-19 22:35:27 +0300

    Create Repository and Contacts Recycler View (#201)
    * Create AppDB and ContactsDao
    
    * Clear database on sign out
    
    * Display contact list using a RecyclerView
    
    * Upgrade build.gradle to newer versions
    
    * Create ContactsRepository and connect to ViewModel
    
    * Work with viewModel in activities
    
    * Make contacts clickable

---
+++ android-client/app/src/main/java/com/example/makore/entities/ContactsDao.java
@@ -0,0 +1,31 @@
+package com.example.makore.entities;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ContactsDao {
+    @Query("SELECT * FROM contact")
+    List<Contact> index();
+
+    // Get all messages
+    @Query("SELECT * FROM message")
+    List<Message> getMessages();
+
+    // Get contact by id
+    @Query("SELECT * FROM contact WHERE id = :id")
+    Contact getContact(String id);
+
+    // Get all messages between contact and current user
+    @Query("SELECT * FROM message WHERE contactId = :id ORDER BY id")
+    List<Message> getAllMessagesWithContact(String id);
+
+    @Insert
+    void insertContact(Contact... contacts);
+
+    @Insert
+    void insertMessage(Message... messages);
+}
